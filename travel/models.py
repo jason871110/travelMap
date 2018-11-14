18a9ea68bcdb120718ca9fa5b3118494a1c9d265
csLesterChen <leochen971008@gmail.com>
2018-11-14 17:28:43 +0800

    basic_database

---
+++ travel/models.py
@@ -0,0 +1,39 @@
+
+# Create your models here.
+from __future__ import unicode_literals
+from django.db import models
+
+
+# Create your models here.
+class Schedule(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100,blank=True)
+    days = models.IntegerField(blank=True)
+    schedule_content = models.TextField(blank=True)
+    location = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class TotalCourse(models.Model):
+    day = models.IntegerField(blank=True)
+    site_content = models.TextField(blank=True)
+    course = models.ForeignKey(Schedule)
+
+    def __str__(self):
+        return self.day
+
+class TouristSite(models.Model):
+    route_order = models.IntegerField()
+    site_name = models.CharField(max_length=100)
+    time =  models.TextField(blank=True)
+    image = models.ImageField(upload_to='upload',blank=True)
+    line = models.ForeignKey(TotalCourse)
+
+    def __str__(self):
+        return self.site_name
+
+class IMG(models.Model):
+    img = models.ImageField(upload_to='upload')
