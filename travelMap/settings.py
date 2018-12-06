bf50665352922a07477c93be84590ab954147098
csLesterChen <leochen971008@gmail.com>
2018-12-06 12:55:17 +0800

    把初步版本結合

--- travelMap/settings.py
+++ travelMap/settings.py
@@ -40,12 +40,11 @@ INSTALLED_APPS = (
     'travel',
 )
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
@@ -64,7 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-            ],
+                ],
         },
     },
 ]
@@ -101,6 +100,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 
 MEDIA_URL = '/media/'
--- travelMap/urls.py
+++ travelMap/urls.py
@@ -13,16 +13,23 @@ Including another URLconf
     1. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import include, url
+from django.urls import path
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
-from travel.views import uploadImg,showImg,insertNewSchedule,addCourseLines,addNewScheduleToDatabase
-
+from travel.views import uploadImg, showImg, sch, main,insertNewSchedule, addCourseLines, addNewScheduleToDatabase, ind,extract_article
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', uploadImg, name=home),
-    url(r'^upload', uploadImg, name=home),
+    url(r'^admin/', admin.site.urls),
+    url(r'^$', uploadImg),
+    url(r'^upload', uploadImg),
     url(r'^show', showImg),
     url(r'^insert', insertNewSchedule),
     url(r'^addline', addCourseLines),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # url(r'^index.(?P<file_type>\w+)/$', ind),
+    path('index', ind, name='index'),
+    url(r'ns',sch),
+    url(r'^article', extract_article),
+    #path('schedule/<int:id_num>', schedule, name='schedule'),
+    path('main/<int:id_num>', main, name='main'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL)
+
